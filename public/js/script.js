ee1833ed8137f7f59e24cc51568957085002af00
jb-ochinang <jb.ochinang@gmail.com>
2016-08-16 13:29:43 +0800

    put back set interval

--- public/js/script.js
+++ public/js/script.js
@@ -57,4 +57,11 @@ $(document).ready(function()
 	$('.wrap').animate({
 		opacity: 1,
 	}, 100);
+	
+	var scrollBar = setInterval(function(){
+		if($('body').children('#qLoverlay').length < 1) {
+		$('body').css('overflow', 'visible');
+		clearInterval(scrollBar);
+		}
+	}, 10);
 });
